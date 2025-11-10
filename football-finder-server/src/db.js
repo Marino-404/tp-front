7c43204816fa929fb14b7f413438864c41532696
Juan Marino Rova <maarino89@hotmail.com>
2025-11-10 14:32:46 -0300

    Borrado server

--- football-finder-server/src/db.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize'
-
-export const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './football-finder.db',
-    dialectOptions: {
-        foreignKeys: true
-    }
-})
